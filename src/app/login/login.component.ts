651a8dbdc32763c22d1b82c73dfdafb5ad9cf9b1
soniagkalliath <soniageorge111@gmail.com>
2022-04-01 13:27:20 +0530

    One way Binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  aim="Your Perfect Banking Partner"
+  accnum="Account Number Plaease!!!"
+  acno=""
+  pswd=""
+
+  //database
+  database = {
+    1000:{acno:1000,uname:"Neer",password:1000,balance:5000},
+    1001:{acno:1001,uname:"Laisha",password:1001,balance:3000},
+    1002:{acno:1002,uname:"Vyom",password:1002,balance:4000}
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  acnoChange(event:any){
+this.acno = event.target.value
+console.log(this.acno)
+  }
+
+  pswdChange(event:any){
+    this.pswd = event.target.value
+    console.log(this.pswd)
+      }
+
+
+  login(){
+    alert("Login Clicked!!!!")
+  }
+
 
 }
