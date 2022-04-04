8bb6c4c6023c2e92577e1a87584083bc1f74595a
soniagkalliath <soniageorge111@gmail.com>
2022-04-04 11:05:31 +0530

    event biniding using

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
   pswd=""
 
   //database
-  database = {
+  database:any = {
     1000:{acno:1000,uname:"Neer",password:1000,balance:5000},
     1001:{acno:1001,uname:"Laisha",password:1001,balance:3000},
     1002:{acno:1002,uname:"Vyom",password:1002,balance:4000}
@@ -34,9 +34,28 @@ console.log(this.acno)
     console.log(this.pswd)
       }
 
-
+//login - using event binding
   login(){
-    alert("Login Clicked!!!!")
+   //user entered acno n pswd
+   var acno = this.acno
+   var pswd = this.pswd
+
+   let database = this.database
+
+   if(acno in database){
+
+    if(pswd == database[acno]["password"]){
+
+      alert("Login successful!!!!")
+    }
+    else{
+      alert("Incorrect password!!!")
+    }
+
+   }
+   else{
+     alert("User doesnot exist!!!!")
+   }
   }
 
 
